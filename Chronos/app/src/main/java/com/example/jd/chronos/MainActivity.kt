9377804978ef772878fdce5263b896eecfc651cf
Enrique Juan de Dios <enrique.juandedios@avantica.net>
2017-11-22 12:20:16 -0500

    Added Basic ViewModel Kotlin Implementation

--- Chronos/app/src/main/java/com/example/jd/chronos/MainActivity.kt
+++ Chronos/app/src/main/java/com/example/jd/chronos/MainActivity.kt
@@ -1,15 +1,32 @@
 package com.example.jd.chronos
 
+import android.arch.lifecycle.Observer
+import android.arch.lifecycle.ViewModelProviders
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.SystemClock
 import android.widget.Chronometer
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Setup ViewModel
+        val model = ViewModelProviders.of(this).get(MainViewModel::class.java)
+
         val c:Chronometer = findViewById (R.id.chronometer)
+
+        if(model.startTime.equals(0)){
+            val startTime = SystemClock.elapsedRealtime()
+            model.startTime = startTime
+            chronometer.base = startTime
+
+        }else{
+            chronometer.base = model.startTime
+        }
         c.start()
     }
 }
---
+++ Chronos/app/src/main/java/com/example/jd/chronos/MainViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.jd.chronos
+
+import android.arch.lifecycle.ViewModel
+
+//import android.arch.lifecycle
+/**
+ * Created by jd on 11/22/17.
+ */
+class MainViewModel:ViewModel(){
+
+    var startTime:Long= 0
+
+}
