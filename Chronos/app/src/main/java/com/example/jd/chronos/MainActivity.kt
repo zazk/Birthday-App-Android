ca7230e8977a1ce66e3ccc1c1d737c0d90a90f9c
zazk <juandedioz@gmail.com>
2017-12-02 07:26:25 -0500

    Chronos & Timer running fluently

--- Chronos/app/src/main/java/com/example/jd/chronos/MainActivity.kt
+++ Chronos/app/src/main/java/com/example/jd/chronos/MainActivity.kt
@@ -5,17 +5,22 @@ import android.arch.lifecycle.ViewModelProviders
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.SystemClock
+import android.util.Log
 import android.widget.Chronometer
+import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
+
+
 
 class MainActivity : AppCompatActivity() {
 
+    var model:MainViewModel = MainViewModel()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         // Setup ViewModel
-        val model = ViewModelProviders.of(this).get(MainViewModel::class.java)
+        model = ViewModelProviders.of(this).get(MainViewModel::class.java)
 
         val c:Chronometer = findViewById (R.id.chronometer)
 
@@ -28,5 +33,16 @@ class MainActivity : AppCompatActivity() {
             chronometer.base = model.startTime
         }
         c.start()
+        subscribe()
+    }
+
+    private fun subscribe() {
+        val elapsedTimeObserver = Observer<Long> { aLong ->
+            val newText:String = this.resources.getString(R.string.seconds,aLong)
+            timer_textview.text = newText
+            Log.d("Chrono Activity", "Updating timer")
+        }
+
+        model.mTimer.observe(this, elapsedTimeObserver)
     }
 }
--- Chronos/app/src/main/java/com/example/jd/chronos/MainViewModel.kt
+++ Chronos/app/src/main/java/com/example/jd/chronos/MainViewModel.kt
@@ -1,13 +1,30 @@
 package com.example.jd.chronos
 
 import android.arch.lifecycle.ViewModel
+import android.arch.lifecycle.MutableLiveData
+import android.os.SystemClock
+import java.util.*
+import kotlin.concurrent.timerTask
 
 //import android.arch.lifecycle
 /**
  * Created by jd on 11/22/17.
  */
-class MainViewModel:ViewModel(){
+class MainViewModel:ViewModel{
 
-    var startTime:Long= 0
+    var startTime:Long
+    var elapsedTime:Long = 0
+    val ONE_SECOND:Int = 5000
+    val mTimer:MutableLiveData<Long> = MutableLiveData()
+
+    constructor(){
+        startTime = SystemClock.elapsedRealtime()
+        val timer:Timer = Timer()
+        timer.scheduleAtFixedRate(timerTask {
+            elapsedTime = (SystemClock.elapsedRealtime() - startTime)/1000
+            mTimer.postValue(elapsedTime)
+        },ONE_SECOND.toLong(),ONE_SECOND.toLong())
+    }
+
 
 }
