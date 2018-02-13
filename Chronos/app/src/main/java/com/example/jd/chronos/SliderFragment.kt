dfd7934db7d3318da12c83aee2d5d46b2ed20b86
Enrique Juan de Dios <juandedioz@gmail.com>
2018-02-13 00:08:33 -0500

    Added ViewModel to Fragment. Tested Loading

--- Chronos/app/src/main/java/com/example/jd/chronos/SliderFragment.kt
+++ Chronos/app/src/main/java/com/example/jd/chronos/SliderFragment.kt
@@ -4,10 +4,13 @@ import android.content.Context
 import android.net.Uri
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SeekBar
 
+import kotlinx.android.synthetic.main.fragment_slider.*
 
 /**
  * A simple [Fragment] subclass.
@@ -19,16 +22,33 @@ import android.view.ViewGroup
  */
 class SliderFragment : Fragment() {
 
-    // TODO: Rename and change types of parameters
-    private var mParam1: String? = null
-    private var mParam2: String? = null
+    private var seekBar: SeekBar? = null
 
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
+        seekBar = seekBarX
+
+        subscribeSeekBar()
         return inflater!!.inflate(R.layout.fragment_slider, container, false)
     }
+
+
+
+    private fun subscribeSeekBar(){
+        seekBar?.setOnSeekBarChangeListener( object :SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar, progress:Int, fromUser:Boolean ){
+                Log.d("Slider Fragment:","onProgressChanged")
+            }
+            override fun onStartTrackingTouch(seekBar: SeekBar){}
+
+            override fun onStopTrackingTouch(seekBar: SeekBar){}
+        })
+    }
+
+
+
 
 
 }// Required empty public constructor
---
+++ Chronos/app/src/main/java/com/example/jd/chronos/SliderViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.jd.chronos
+
+import android.arch.lifecycle.MutableLiveData
+import android.arch.lifecycle.ViewModel
+
+/**
+ * Created by user on 2/12/18.
+ */
+class SliderViewModel:ViewModel()  {
+
+    var sliderBarValue = MutableLiveData<Int>();
+
+}
